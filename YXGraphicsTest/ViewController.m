a4ddc3db16707dd01f5eaa8ee9366eab0462584f
yaohongxiao <>
2020-07-07 15:44:34 +0800

    change：up

--- YXGraphicsTest/ViewController.m
+++ YXGraphicsTest/ViewController.m
@@ -42,7 +42,31 @@
 #pragma mark - 第二种图形方法
 - (void)methodSecond {
     
-    NSMutableArray *arr = [YXPieChartLineGraphicsArrModel arrayOfModelsFromDictionaries:@[@{kPieChartLineGraphicsName:@"1", kPieChartLineGraphicsValue:@(10)}, @{kPieChartLineGraphicsName:@"2", kPieChartLineGraphicsValue:@(10)}, @{kPieChartLineGraphicsName:@"3", kPieChartLineGraphicsValue:@(10)}, @{kPieChartLineGraphicsName:@"4", kPieChartLineGraphicsValue:@(10)}]];
+    NSMutableArray *arr = [YXPieChartLineGraphicsArrModel arrayOfModelsFromDictionaries:@[
+                           @{kPieChartLineGraphicsName:@"1", kPieChartLineGraphicsValue:@(10)},
+                           @{kPieChartLineGraphicsName:@"2", kPieChartLineGraphicsValue:@(10)},
+                           @{kPieChartLineGraphicsName:@"3", kPieChartLineGraphicsValue:@(10)},
+                           @{kPieChartLineGraphicsName:@"4", kPieChartLineGraphicsValue:@(22)},
+                           @{kPieChartLineGraphicsName:@"5", kPieChartLineGraphicsValue:@(10)},
+                           @{kPieChartLineGraphicsName:@"6", kPieChartLineGraphicsValue:@(10)},
+                           @{kPieChartLineGraphicsName:@"7", kPieChartLineGraphicsValue:@(15)},
+                           @{kPieChartLineGraphicsName:@"8", kPieChartLineGraphicsValue:@(10)},
+                           @{kPieChartLineGraphicsName:@"9", kPieChartLineGraphicsValue:@(10)},
+                           @{kPieChartLineGraphicsName:@"0", kPieChartLineGraphicsValue:@(10)},
+                           @{kPieChartLineGraphicsName:@"10", kPieChartLineGraphicsValue:@(10)},
+                           @{kPieChartLineGraphicsName:@"11", kPieChartLineGraphicsValue:@(30)},
+                           @{kPieChartLineGraphicsName:@"12", kPieChartLineGraphicsValue:@(10)},
+                           @{kPieChartLineGraphicsName:@"13", kPieChartLineGraphicsValue:@(77)},
+                           @{kPieChartLineGraphicsName:@"14", kPieChartLineGraphicsValue:@(10)},
+                           @{kPieChartLineGraphicsName:@"15", kPieChartLineGraphicsValue:@(64)},
+                           @{kPieChartLineGraphicsName:@"16", kPieChartLineGraphicsValue:@(10)},
+                           @{kPieChartLineGraphicsName:@"17", kPieChartLineGraphicsValue:@(10)},
+                           @{kPieChartLineGraphicsName:@"18", kPieChartLineGraphicsValue:@(10)},
+                           @{kPieChartLineGraphicsName:@"19", kPieChartLineGraphicsValue:@(10)},
+                           @{kPieChartLineGraphicsName:@"20", kPieChartLineGraphicsValue:@(10)},
+                           @{kPieChartLineGraphicsName:@"21", kPieChartLineGraphicsValue:@(10)},
+                           @{kPieChartLineGraphicsName:@"22", kPieChartLineGraphicsValue:@(10)},
+                           @{kPieChartLineGraphicsName:@"23", kPieChartLineGraphicsValue:@(10)},]];
     
     YXPieChartLineGraphicsView *view = [[YXPieChartLineGraphicsView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 500)];
     view.dataSourceArr = arr;
